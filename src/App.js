04d1285e89f973c5255952b85854fa0bbb93abaa
RezaulAlamOni <rzalam93@gmail.com>
2020-08-31 04:20:12 +0600

    conditional rendaring

--- src/App.js
+++ src/App.js
@@ -7,24 +7,27 @@ import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
 import Message from "./components/Message";
 import Counter from "./components/Counter";
+import UserGreeting from "./components/UserGreeting";
 
 function App() {
     return (
         <div className="App">
             {/*<Greet name="Oni"/>*/}
-            <Greet name="Rezaul" herroName="HErro">
-                <p>This is children props</p>
-            </Greet>
-            <Welcome name="Messi" herroName="Football">
-                <a href="/"> THis is children prop 1</a>
-            </Welcome>
+            {/*<Greet name="Rezaul" herroName="HErro">*/}
+            {/*    <p>This is children props</p>*/}
+            {/*</Greet>*/}
+            {/*<Welcome name="Messi" herroName="Football">*/}
+            {/*    <a href="/"> THis is children prop 1</a>*/}
+            {/*</Welcome>*/}
             {/*<Welcome name="Nadal" herroName="Tennis">*/}
             {/*    <a href="/"> THis is children prop 2</a><br/>*/}
             {/*    <button>Click Here</button>*/}
             {/*</Welcome>*/}
             {/*<Hello/>*/}
-            <Message />
-            <Counter/>
+            {/*<Message />*/}
+            {/*<Counter/>*/}
+
+            <UserGreeting />
 
         </div>
     );
---
+++ src/components/UserGreeting.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+
+class UserGreeting extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            isLoggedIn : true
+        }
+    }
+
+    render() {
+        //if else block
+        // if (this.state.isLoggedIn) {
+        //     return <div><h1>Welcome Visitor</h1></div>
+        // } else {
+        //     return  <div><h1>Welcome guest</h1></div>
+        // }
+        //  use variable
+        // let message;
+        // if (this.state.isLoggedIn) {
+        //     message = <div><h1>Welcome Visitor</h1></div>
+        // } else {
+        //     message = <div><h1>Welcome guest</h1></div>
+        // }
+        //
+        // return <div>{message}</div>
+
+        // user ternary operator
+        // return this.state.isLoggedIn ?
+        //     <div><h1>Welcome Visitor</h1></div> :
+        //     <div><h1>Welcome guest</h1></div>
+
+        // user sort circuit operator approach
+
+        return this.state.isLoggedIn && <div><h1>Welcome Visitor</h1></div>
+    }
+}
+
+export default UserGreeting;
